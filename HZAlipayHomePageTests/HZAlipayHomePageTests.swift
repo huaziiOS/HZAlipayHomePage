74fd8b1f44872bd2afd5a4b0a1af78bb1d1c495a
倔强的华子 <hanzhaohua1992@163.com>
2017-08-25 10:06:39 +0800

    Initial Commit

---
+++ HZAlipayHomePageTests/HZAlipayHomePageTests.swift
@@ -0,0 +1,36 @@
+//
+//  HZAlipayHomePageTests.swift
+//  HZAlipayHomePageTests
+//
+//  Created by 韩兆华 on 2017/8/25.
+//  Copyright © 2017年 韩兆华. All rights reserved.
+//
+
+import XCTest
+@testable import HZAlipayHomePage
+
+class HZAlipayHomePageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
